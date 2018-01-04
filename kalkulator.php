a19c17dd0121e8099d2b62bad7e685d2e1b99f7b
nole999 <dragan1945@gmail.com>
2018-01-04 23:17:17 +0100

    first commit

---
+++ kalkulator.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Kalkulator</title>
+</head>
+<body>
+<form> 
+          <input type="text" name="num1" placeholder="Prvi broj">
+          <input type="text" name="num2" placeholder="Drugi broj">
+          <select name="operator">
+             <option>Nista</option>
+             <option>Sabiranje</option>
+             <option>Oduzimanje</option>
+             <option>Mnozenje</option>
+             <option>Deljenje</option>
+          </select>
+          <br>
+          <button type="sumbit" name="sumbit" value="sumbit">Izracunaj</button>
+
+</form>
+
+<p>Odgovor je:</p>
+<?php
+     if (isset($_GET['sumbit'])) {
+          $result1 = $_GET['num1'];
+          $result2 = $_GET['num2'];
+          $operator = $_GET['operator'];
+          switch ($operator) {
+               case "Nista":
+               echo "GRESKA!Izaberi operaciju.";
+               break;
+               case "Sabiranje":
+               echo $result1 + $result2;
+               break;
+               case "Oduzimanje":
+               echo $result1 - $result2;
+               break;
+               case "Mnozenje":
+               echo $result1 * $result2;
+               break;
+               case "Deljenje":
+               echo $result1 / $result2;
+               break;
+          }
+        }
+?>
+</body>
+</html>
